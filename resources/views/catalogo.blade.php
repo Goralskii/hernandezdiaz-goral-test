4a9a1628bc9206fc19888eca5c7437e9fd0f796f
Goralskii <nicoegoral@gmail.com>
2026-04-17 16:54:26 -0300

    Carrusel y mas categorias en el navbar
    se implemento un carrusel generico con 3 imagenes y se completo con mas pestañas el navbar

---
+++ resources/views/catalogo.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> <html>
+    <head> 
+        <title>Home</title>
+
+        <link rel="stylesheet" href="{{asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    </head> 
+
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container">
+              <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('Assets/logo.jpeg') }}" alt="Aura Store" width="45" class="d-inline-block align-text-top me-2">
+                    Aura Store
+                </a>
+              <div class="navbar-nav">
+                <a class="nav-link active" href="/quienes-somos">Quiénes somos</a>
+                <a class="nav-link active" href="/comercializacion">Comercialización</a>
+                <a class="nav-link active" href="/catalogo">Catalogo</a>
+                <a class="nav-link active" href="/contacto">Contacto</a>
+                <a class="nav-link active" href="/consultas">Consultas</a>
+                <a class="nav-link active" href="/terminos-y-condiciones">Términos y condiciones</a>
+                <a class="nav-link active" href="/sobre-mi">Sobre mí</a>
+                </div>
+            </div>
+        </nav>
+
+        <p> ESTAS EN LA VENTANA DE CATALOGO </p>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/sobre-mi', function () { return view('sobre-mi'); });
+Route::get('/catalogo', function () { return view('catalogo'); });
+Route::get('/consultas', function () { return view('consultas'); });
+Route::get('/contacto', function () { return view('contacto'); });
+Route::get('/terminos-y-condiciones', function () { return view('terminos-y-condiciones'); });
+Route::get('/quienes-somos', function () { return view('quienes-somos'); });
+Route::get('/comercializacion', function () { return view('comercializacion'); });
